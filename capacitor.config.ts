e1c86627a95cb2d3c2911791ac587ab9196abbc3
Mani <manikandanfullstackdeveloper@gmail.com>
2023-03-22 22:50:17 +0530

    nme chnage

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.sjc.studuentforum',
-  appName: 'Student Forum',
+  appName: 'College Forum',
   webDir: 'www',
   bundledWebRuntime: false,
 };
